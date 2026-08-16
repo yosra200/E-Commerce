bbcfbdc2df091c43eeb4f169c3414e962488bb8b
yosra200 <yosrakhattab2020@gmail.com>
2026-08-16 13:15:35 +0300

    update

--- app/Http/Controllers/Api/FavoriteController.php
+++ app/Http/Controllers/Api/FavoriteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\traits\ApiResponse;
+use App\Traits\ApiResponse;
 use App\Models\Favorite;
 use App\Http\Requests\AddFavoriteRequest;
 
