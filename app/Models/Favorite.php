66fe59c4e950cf2768107958b2fb6ca307fa775d
yosra200 <yosrakhattab2020@gmail.com>
2026-08-16 13:18:33 +0300

    update

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -10,4 +10,14 @@ class Favorite extends Model
         'user_id',
         'product_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
